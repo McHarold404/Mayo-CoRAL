c05f6d46ba4e0f570f45135cf1d920141112a7ad
McHarold404 <namanahuja0404@gmail.com>
2025-02-26 23:17:38 -0700

    added modular inference

---
+++ LLM_Extraction_and_CrossCritique/Code/chunk_retreive_tabulate.py
@@ -0,0 +1,9 @@
+
+### chunk documents
+
+### speculative retrieval:
+    ## iterate over all categories
+    ###speculative retrieval from all variables
+    ## evaluate against gold table
+
+### speculative
